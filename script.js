d154ace2e3515cdc0f680bf868f048e0c17c0354
Ferrarinix <112392375+Ferrarinix@users.noreply.github.com>
2024-08-20 09:15:18 -0300

    20.08

--- script.js
+++ script.js
@@ -6,15 +6,15 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: Qual é o nome completo do personagem principal de Breaking Bad?",
+        enunciado: "Qual é o nome completo do personagem principal de Breaking Bad?",
         alternativas: [
             {
                 texto: "Walter White",
-                afirmacao: "afirmação"
+                correta: true
             },
             {
                 texto: "Gustavo Fring",
-                afirmacao: "afirmação"
+                correta: false
             }
         ]
     },
@@ -22,56 +22,55 @@ const perguntas = [
         enunciado: "Qual é a doença terminal que Walter White é diagnosticado no início da série?",
         alternativas: [
             {
-                texto: "Câncer de estomago",
-                afirmacao: "afirmação"
+                texto: "Câncer de estômago",
+                correta: false
             },
             {
                 texto: "Câncer de pulmão",
-                afirmacao: "afirmação"
+                correta: true
             }
         ]
     },
     {
-        enunciado: "Qual é o apelido criador por Walter White para manter sua identidade escondida?",
+        enunciado: "Qual é o apelido criado por Walter White para manter sua identidade escondida?",
         alternativas: [
             {
-                texto: "Mr.cook",
-                afirmacao: "afirmação"
+                texto: "Mr. Cook",
+                correta: false
             },
             {
                 texto: "Heisenberg",
-                afirmacao: "afirmação"
+                correta: true
             }
         ]
     },
     {
-        enunciado: "Qual é o nome do advogado criminal que se torna conhecido por seu bordão "Better Call Saul"?",
+        enunciado: 'Qual é o nome do advogado criminal que se torna conhecido por seu bordão "Better Call Saul"?',
         alternativas: [
             {
-                texto: "Jimmy McGill.",
-                afirmacao: "afirmação"
+                texto: "Jimmy McGill",
+                correta: false
             },
             {
                 texto: "Saul Goodman",
-                afirmacao: "afirmação"
+                correta: true
             }
         ]
     },
     {
-        enunciado: "Qual o nome do spin-off de Breaking Bad? ",
+        enunciado: "Qual o nome do spin-off de Breaking Bad?",
         alternativas: [
             {
                 texto: "El Camino",
-                afirmacao: "afirmação"
+                correta: false
             },
             {
                 texto: "Better Call Saul",
-                afirmacao: "afirmação"
+                correta: true
             }
         ]
-    },
+    }
 ];
-
 
 let atual = 0;
 let perguntaAtual;
@@ -89,23 +88,26 @@ function mostraPergunta() {
 }
 
 function mostraAlternativas(){
-    for(const alternativa of perguntaAtual.alternativas) {
-        const botaoAlternativas = document.createElement("button");
-        botaoAlternativas.textContent = alternativa.texto;
-        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
-        caixaAlternativas.appendChild(botaoAlternativas);
+    for (const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativa = document.createElement("button");
+        botaoAlternativa.textContent = alternativa.texto;
+        botaoAlternativa.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativa);
     }
 }
 
 function respostaSelecionada(opcaoSelecionada) {
-    const afirmacoes = opcaoSelecionada.afirmacao;
-    historiaFinal += afirmacoes + " ";
+    if (opcaoSelecionada.correta) {
+        historiaFinal += "Resposta correta! ";
+    } else {
+        historiaFinal += "Resposta incorreta. ";
+    }
     atual++;
     mostraPergunta();
 }
 
 function mostraResultado() {
-    caixaPerguntas.textContent = "Em 2049...";
+    caixaPerguntas.textContent = "Fim do Quiz!";
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
 }
